c6fb88158edf242a453917338a5d80e337e69f02
jenningsnickolas <jenningsnickolas@gmail.com>
2015-01-02 14:53:45 -0500

    dateModified now displays 2pm instead of 14

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 // initialize the counter and the array
 var versionNum = 0.05;
-compileDate = new Date("12,29,2014");
+compileDate = "12-29-2014";
 
 function printFooter() {
-	document.write("<footer><p style=\"float:right\"><strong>AJ Handmade Furniture</strong> &copy 2014</p>" +
-			"<p style=\"text-align:left\"><strong>V " + versionNum + "</strong> Compiled " + 
-			(compileDate.getMonth()+1) + "-" + compileDate.getDate() + "-" + compileDate.getFullYear() + "</p></footer>");
+	document.write("<footer><p style=\"float:right\"><strong>AJ Handmade Furniture</strong> &copy 2014</p>" + "<p style=\"text-align:left\"><strong>V " + versionNum + "</strong> Compiled " + compileDate + "</p></footer>");
 }
 
 function printLastModifiedDate() {
 	var lastDate = new Date(document.lastModified);
 	var minutes = lastDate.getMinutes();
+	var hours = lastDate.getHours();
+	if (hours > 12) {
+		hours = hours - 12;
+		varTimeOfDay = "PM";
+	} else {
+		varTimeOfDay = "AM";
+	}
 	if (minutes < 10) {
 		minutes = "0" + minutes;
 	}
-	var stringDate = (lastDate.getMonth()+1) + "-" + lastDate.getDate() + "-" + lastDate.getFullYear() + " " + lastDate.getHours() + ":" + minutes;
+	var stringDate = (lastDate.getMonth()+1) + "-" + lastDate.getDate() + "-" + lastDate.getFullYear() + " " + hours + ":" + minutes + " " + varTimeOfDay;
 	document.write("Page last modified " + stringDate);
 }
